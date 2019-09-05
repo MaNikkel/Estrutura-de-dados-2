38f19fb6ced58a4d0b9df054f25ccfbd872a9e09
Mathias Nikkel <mathiasnikkel@gmail.com>
2019-09-05 20:10:27 -0300

    Recursividade parte 2

--- recursividade_2/main.c
+++ recursividade_2/main.c
@@ -12,18 +12,45 @@ int search(int elem, int first, int last, int *array){
         printf("\n----------------\nindex = %d", first);
         return *(array);
     }
-    search(elem, ++first, last, (array+1));
+    return search(elem, ++first, last, (array+1));//array+1 aponta para o próximo elemento
+    //nesse return acontece o empilhamento
 }
 
+int search2(int elem, int first, int last, int *array){
+    if(first > last){
+        printf("\n----------------\n");
+        return NULL;
+    }
+
+    int middle = (first+last)/2;
+
+    if(*(array+middle) == elem){
+        printf("\n----------------\nindex = %d", middle);
+        return middle;
+    }
+
+    //pesquisa em toda a primeira metade,por empilhamento
+    int ind = search2(elem, first, middle-1, array);
+    if(!ind){
+        //se não estiver na primeira metade, aí busca na segunda
+        return search2(elem, middle+1, last, array);
+    } else {
+        return *(array+ind);
+    }
+}
 
 int main() {
     int arr[SIZE];
     int i;
     for(i = 0;i < SIZE; i++){
-        arr[i] = rand() % SIZE;
-        printf("%d: %d\n", i, arr[i]);
+        arr[i] = i;//rand() % SIZE;
+        //printf("%d: %d\n", i, arr[i]);
     }
-    int number = search(123, 0, 1000, arr);
+    int elem;
+    printf("Entre com o número a ser encontrado: ");
+    scanf("%d", &elem);
+
+    int number = search2(elem, 0, SIZE, arr);//arr é o endereço para arr[0]
     if(number){
         printf("\nnúmero encontrado: %d\n\n", number);
     }else{
